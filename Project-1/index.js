29ef8c9e731145d9163b216cab4a2c2d77e519a2
BharatKhatwani <bharatkhatwani06@gmail.com>
2024-08-29 01:36:09 +0530

    "project_2"

--- Project-1/index.js
+++ Project-1/index.js
@@ -7,10 +7,12 @@ addButton.addEventListener("click", function() {
     if(count == 0) {
         nameElement.innerHTML = 'Friends';
         nameElement.style.color = "green";
+        addButton.innerHTML = "Remove Friends";
         count = 1;
     } else {
         nameElement.innerHTML = 'Strangers';
         nameElement.style.color = 'red';
+        addButton.innerHTML = "Add Friends";
         count = 0;
     }
 });
